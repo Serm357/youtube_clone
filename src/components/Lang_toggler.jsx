15797c17acfcec96fef13408d0711862f7e958f2
Serm357 <kilangalilasalum@gmail.com>
2023-12-03 18:06:22 +0300

    initial commit

---
+++ src/components/Lang_toggler.jsx
@@ -0,0 +1,5 @@
+// import { categories } from "../utils/constants";
+// const Lang_toggler = () => {
+//   return <div>Lang_toggler</div>;
+// };
+// export default Lang_toggler;
---
+++ src/utils/constants.js
@@ -0,0 +1,61 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "Mpya", icon: <HomeIcon /> },
+  { name: "Habari", icon: <LiveTvIcon /> },
+  { name: "Teknolojia", icon: <DeveloperModeIcon /> },
+  { name: "Millard Ayo", icon: <LiveTvIcon /> },
+  { name: "Programming", icon: <CodeIcon /> },
+  { name: "Taaluma", icon: <SchoolIcon /> },
+  { name: "Dar News TV", icon: <LiveTvIcon /> },
+  { name: "Redio", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Burudani", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Yanga Sc", icon: <GraphicEqIcon /> },
+  { name: "Simba Sc", icon: <GraphicEqIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Urembo", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Vichekesho", icon: <TheaterComedyIcon /> },
+  { name: "jotiTV", icon: <TheaterComedyIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+];
+
+// export const swahiliCategories = [
+//   { name: "Mpya", icon: <LiveTvIcon /> },
+//   { name: "Habari", icon: <LiveTvIcon /> },
+//   { name: "Teknolojia", icon: <DeveloperModeIcon /> },
+//   { name: "Millard Ayo", icon: <LiveTvIcon /> },
+//   { name: "Programming", icon: <CodeIcon /> },
+//   { name: "Taaluma", icon: <SchoolIcon /> },
+//   { name: "Redio", icon: <GraphicEqIcon /> },
+//   { name: "Movie", icon: <OndemandVideoIcon /> },
+//   { name: "Burudani", icon: <SportsEsportsIcon /> },
+//   { name: "Live", icon: <LiveTvIcon /> },
+//   { name: "Yanga Sc", icon: <GraphicEqIcon /> },
+//   { name: "Simba Sc", icon: <GraphicEqIcon /> },
+//   { name: "Fashion", icon: <CheckroomIcon /> },
+//   { name: "Urembo", icon: <FaceRetouchingNaturalIcon /> },
+//   { name: "Vichekesho", icon: <TheaterComedyIcon /> },
+//   { name: "Music", icon: <MusicNoteIcon /> },
+// ];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+export const demoChannelTitle = "Millard Ayo Tv";
+export const demoVideoTitle = " pata Habari zote mpya kidigitali";
+export const demoProfilePicture = " ";
